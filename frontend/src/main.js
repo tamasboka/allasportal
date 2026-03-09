51cecd413bbc8452a74fbf5108ddfdd4a44421ea
BBence1 <beretzky.beni@gmail.com>
2026-03-09 17:49:42 +0100

    imported style.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../public/style.css'
 
 const app = createApp(App)
 

3bd276add9f243f0f698314234fb558318b9fcdd
BBence1 <beretzky.beni@gmail.com>
2026-03-09 13:55:13 +0100

    imported Navbar, and made a Navbar and RouterView in template

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<script>
+
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "AppLayout",
+  components: {Navbar}
+}
+</script>
+
+<template>
+  <Navbar/>
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>

0d58f29a3b2f312a02f7322b0677ecbb3a54dbc9
BBence1 <beretzky.beni@gmail.com>
2026-03-09 17:49:19 +0100

    made an input and button

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,6 +10,11 @@ export default {
       <div class="box text-center">
         <h1>Jobnest</h1>
         <p>A Jobnest lehetővé teszi a felhasználók számára az egyszerű munka keresést, vagy annak közzétételét.</p>
+        <div class="d-inline-flex">
+          <input class="w-75 form-control" type="text" placeholder="Itt kereshetsz">
+          <button class="btn btn-secondary">Keresés</button>
+        </div>
+
       </div>
     </div>
   </section>

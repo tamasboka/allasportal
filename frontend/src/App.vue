edf2cf097edb3736402a780bd95691953d881de7
BBence1 <beretzky.beni@gmail.com>
2026-03-09 13:53:04 +0100

    created name, components, computed, methods, and RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script>
+export default {
+  name: 'App',
+  components:{},
+  computed:{},
+  methods:{}
+}
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<RouterView/>
 </template>
 
 <style scoped></style>

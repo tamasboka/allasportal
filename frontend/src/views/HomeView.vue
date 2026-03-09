b48be0bcc472d835f31d09aeeaf8c25ff3234dd2
BBence1 <beretzky.beni@gmail.com>
2026-03-09 13:56:23 +0100

    description

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "Home",
+}
+</script>
+
+<template>
+  <section>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <div class="box text-center">
+        <h1>Jobnest</h1>
+        <p>A Jobnest lehetővé teszi a felhasználók számára az egyszerű munka keresést, vagy annak közzétételét.</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>

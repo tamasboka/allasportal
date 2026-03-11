2d782317237717b8949c7b33f0081d09a43f4a93
Bóka Tamás <tomi500offical@gmail.com>
2026-03-11 21:39:24 +0100

    moved from ../

---
+++ frontend/src/views/App/HomeView.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "Home",
+}
+</script>
+
+<template>
+  <section>
+    <div class="d-flex xy-center">
+      <div class="text-center">
+        <h1>JobNest</h1>
+        <p>A JobNest lehetővé teszi a felhasználók számára az egyszerű munka keresést, vagy annak közzétételét.</p>
+        <div class="d-inline-flex">
+          <input class="form-control" type="text" placeholder="Itt kereshetsz">
+          <button class="btn btn-secondary">Keresés</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.xy-center {
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+</style>

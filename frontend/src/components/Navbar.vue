438c1dd7b95fb8be1514107d9b84d5b6361c673c
Bóka Tamás <tomi500offical@gmail.com>
2026-03-12 21:47:04 +0100

    refactor: remove border from navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -7,8 +7,8 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar bg-black border-bottom border-body navbar-expand-lg p-3" data-bs-theme="dark">
-      <div class="container-fluid">
+    <nav class="navbar bg-black navbar-expand-lg p-3" data-bs-theme="dark">
+      <div class="container">
         <RouterLink class="navbar-brand" :to="{name: 'home'}">Home</RouterLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,6 +26,7 @@ export default {
           <div class="d-flex gap-3">
             <RouterLink class="btn btn-primary rounded-pill" :to="{name: 'register'}">Regisztrálás</RouterLink>
             <RouterLink class="btn btn-secondary rounded-pill" :to="{name: 'login'}">Bejelentkezés</RouterLink>
+            <button class="btn" :class="{ 'btn-light':  true}"><i class="bi bi-brightness-high"></i></button>
           </div>
         </div>
       </div>

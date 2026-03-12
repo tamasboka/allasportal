03188753704b9ab031bd78deb9d396f208bd3df1
Rendszergazda <rendszergazda@blathy.info>
2026-03-12 14:58:03 +0100

    feat: admin routes, user routes, auth routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,8 +8,21 @@ import RegisterView from "@/views/Auth/RegisterView.vue";
 import LoginView from "@/views/Auth/LoginView.vue";
 import JobView from "@/views/App/Jobs/JobView.vue";
 import CompanyView from "@/views/App/Companies/CompanyView.vue";
+import AdminLayout from "@/layouts/AdminLayout.vue";
+import AdminHome from "@/views/Admin/AdminHome.vue";
+import AdminCompaniesView from "@/views/Admin/Company/AdminCompaniesView.vue";
+import AdminCompanyActions from "@/views/Admin/Company/AdminCompanyActions.vue";
+import AdminJobsView from "@/views/Admin/Jobs/AdminJobsView.vue";
+import AdminJobActions from "@/views/Admin/Jobs/AdminJobActions.vue";
+import AdminUsersListView from "@/views/Admin/User/AdminUsersListView.vue";
+import AdminUserActions from "@/views/Admin/User/AdminUserActions.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
+import UserHomeView from "@/views/UserHomeView.vue";
+import UserSettings from "@/views/User/Actions/UserSettings.vue";
+import CreateJob from "@/views/User/Actions/CreateJob.vue";
 
 const routes = [
+    // APP
     {
         path: '/',
         component: AppLayout,
@@ -58,10 +71,12 @@ const routes = [
                     }
                 ]
             },
+
         ]
     },
+    // AUTH
     {
-        path: '/auth',
+        path: 'auth',
         component: AuthLayout,
         children: [
             {
@@ -80,6 +95,128 @@ const routes = [
                     title: 'Bejelentkezés'
                 }
             },
+        ]
+    },
+    // ADMIN
+    {
+        path: 'admin',
+        component: AdminLayout,
+        children: [
+            {
+                path: '',
+                component: AdminHome,
+                name: 'admin-home',
+                meta: {
+                    title: 'Admin Home'
+                }
+            },
+            {
+                path: 'companies',
+                children: [
+                    {
+                        path: '',
+                        component: AdminCompaniesView,
+                        name: 'admin-companies',
+                        meta: {
+                            title: 'Admin - Companies'
+                        }
+                    },
+                    {
+                        path: ':companyID',
+                        component: AdminCompanyActions,
+                        name: 'admin-company',
+                        meta: {
+                            title: 'Admin - Company Actions'
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'jobs',
+                children: [
+                    {
+                        path: '',
+                        component: AdminJobsView,
+                        name: 'admin-jobs',
+                        meta: {
+                            title: 'Admin - Jobs'
+                        }
+                    },
+                    {
+                        path: 'jobID',
+                        component: AdminJobActions,
+                        name: ':admin-job',
+                        meta: {
+                            title: 'Admin - Job Actions'
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'users',
+                children: [
+                    {
+                        path: '',
+                        component: AdminUsersListView,
+                        name: 'admin-users',
+                        meta: {
+                            title: 'Admin - Users'
+                        }
+                    },
+                    {
+                        path: ':userID',
+                        component: AdminUserActions,
+                        name: 'admin-user',
+                        meta: {
+                            title: 'Admin - User Actions'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    // USER
+    {
+        path: '/user',
+        component: UserLayout,
+        children: [
+            {
+                path: ':userID',
+                component: UserHomeView,
+                name: 'user-home',
+                meta: {
+                    title: ''
+                }
+            },
+            {
+                path: 'settings',
+                component: UserSettings,
+                name: 'user-settings',
+                meta: {
+                    title: 'Settings'
+                }
+            },
+            {
+                path: 'action',
+                children: [
+                    {
+                        path: 'create-job',
+                        component: CreateJob,
+                        name: 'create-job',
+                        meta: {
+                            title: 'Create Job'
+                        }
+                    },
+                    {
+                        path: 'edit-job',
+                        component: CreateJob,
+                        name: 'edit-job',
+                        meta: {
+                            title: 'Edit Job'
+                        }
+                    }
+                ]
+            }
         ]
     }
 ];

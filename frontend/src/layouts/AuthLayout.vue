7a48d1c2d2b695baeb2a6228f41729a46c2110a1
Bóka Tamás <tomi500offical@gmail.com>
2026-03-13 19:29:11 +0100

    feat: add basic card layout + reactive return to home page button

--- frontend/src/layouts/AuthLayout.vue
+++ frontend/src/layouts/AuthLayout.vue
@@ -1,12 +1,30 @@
 <script>
-
 export default {
-  name: "AuthLayout"
+  name: "AuthLayout",
+  data() {
+    return {
+      isReturnBtnHovered: false
+    }
+  }
 }
 </script>
 
 <template>
-
+  <main>
+    <section class="min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="card text-bg-dark p-1">
+        <div class="card-body">
+          <RouterLink to="/" class="text-decoration-none text-light btn"
+                      :class="{ 'btn-outline-light': !isReturnBtnHovered, 'btn-outline-primary': isReturnBtnHovered}"
+                      @mouseover="isReturnBtnHovered = true" @mouseleave="isReturnBtnHovered = false">
+            <i class="bi bi-arrow-left-circle me-2"></i>
+            Vissza
+          </RouterLink>
+          <RouterView/>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>

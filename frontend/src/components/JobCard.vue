c124da3cd02280b9f61eab8116ec49a7a1949737
BBence1 <beretzky.beni@gmail.com>
2026-03-25 20:48:38 +0100

    a card for one job

---
+++ frontend/src/components/JobCard.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  name: "JobCard",
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <section>
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">{{ job.title }}</h1>
+        <p class="card-text">{{ job.advertiser.firstname }} {{ job.advertiser.lastname }}</p>
+        <RouterLink :to="{name: 'job', params: {jobID: job.id}}">Részletek</RouterLink>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>

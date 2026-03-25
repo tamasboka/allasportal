0c087d1579301ee4649c5c27de1e61c08172aca0
BBence1 <beretzky.beni@gmail.com>
2026-03-25 20:51:15 +0100

    a view for all jobs

--- frontend/src/views/App/Jobs/JobsView.vue
+++ frontend/src/views/App/Jobs/JobsView.vue
@@ -1,12 +1,45 @@
 <script>
+import {getAllJobs} from "@/data/data.js";
+import JobCard from "@/components/JobCard.vue";
 
 export default {
-  name: "Allasok"
+  name: "Allasok",
+  components: {JobCard},
+  data() {
+    return {
+      jobs: [],
+      loading: false,
+      error: ''
+    }
+  },
+  methods: {
+    async LoadAllJobs() {
+      this.loading = true;
+      try {
+        const result = await getAllJobs();
+        this.jobs = result.data;
+      } catch (error) {
+        this.error = error.message
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  mounted() {
+    this.LoadAllJobs();
+  }
 }
 </script>
 
 <template>
+  <section>
+    <div class="row">
+      <div class="col-12 col-xl-2 col-lg-4 col-md-6 col-sm-6" v-for="job in jobs">
+          <JobCard :job="job"/>
+      </div>
+    </div>
 
+  </section>
 </template>
 
 <style scoped>

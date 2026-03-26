b716e472da3040062fbff6ce143b74ec9b193c06
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 20:41:01 +0100

    feat: make index, store, show organization functions

--- backend/app/Http/Controllers/api/OrganizationController.php
+++ backend/app/Http/Controllers/api/OrganizationController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrganizationRequest;
+use App\Http\Resources\Organization\OrganizationCollection;
+use App\Http\Resources\Organization\OrganizationResource;
+use App\Models\Organization;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class OrganizationController extends Controller
@@ -12,15 +17,31 @@ class OrganizationController extends Controller
      */
     public function index()
     {
-        //
+        $all_organizations = Organization::withCount('workers as workers_count')
+            //->with(['workers'])
+            ->get();
+        return (new OrganizationCollection($all_organizations))
+            ->response()
+            ->setStatusCode(200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(OrganizationRequest $request)
     {
-        //
+        if ($request->user()->tokenCan('admin')) {
+            $validated = $request->validated();
+            $organization = Organization::create($validated);
+            return (new OrganizationResource($organization))
+                ->response()
+                ->setStatusCode(201);
+        } else {
+            return response()
+                ->json([
+                    'message' => 'Unauthorized'
+                ], 401);
+        }
     }
 
     /**
@@ -28,7 +49,19 @@ class OrganizationController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $organization = Organization::with(['workers', 'jobs'])
+                ->withCount('workers')
+                ->findOrFail($id);
+            return (new OrganizationResource($organization))
+                ->response()
+                ->setStatusCode(200);
+        } catch (ModelNotFoundException) {
+            return response()
+                ->json([
+                    'message' => 'Organization not found'
+                ], 404);
+        }
     }
 
     /**

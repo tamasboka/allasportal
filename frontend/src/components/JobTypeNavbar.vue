d946c9e71d7b11844e5009492db2b0a153391a65
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 17:08:35 +0100

    create filter for jobs by type

--- frontend/src/components/JobTypeNavbar.vue
+++ frontend/src/components/JobTypeNavbar.vue
@@ -1,13 +1,34 @@
 <script>
 export default {
-  name: "JobTypeNavbar"
+  name: "JobTypeNavbar",
+  methods: {
+    emitFilter(filter) {
+      this.$emit('newFilter', filter)
+    }
+  },
+  emits: ['newFilter']
 }
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <nav class="navbar bg-black navbar-expand-lg" data-bs-theme="dark">
+    <div class="container d-flex justify-content-center">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <button class="nav-link" @click="emitFilter('all')">Összes</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link" @click="emitFilter('fulltime')">Full Time</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link" @click="emitFilter('parttime')">Part Time</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link" @click="emitFilter('onetime')">One Time</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
--- frontend/src/views/App/Jobs/JobsView.vue
+++ frontend/src/views/App/Jobs/JobsView.vue
@@ -1,13 +1,15 @@
 <script>
 import {getAllJobs} from "@/data/data.js";
 import JobCard from "@/components/JobCard.vue";
+import JobTypeNavbar from "@/components/JobTypeNavbar.vue";
 
 export default {
-  name: "Allasok",
-  components: {JobCard},
+  name: "JobsView",
+  components: {JobTypeNavbar, JobCard},
   data() {
     return {
       jobs: [],
+      filteredJobs: [],
       loading: false,
       error: ''
     }
@@ -17,12 +19,19 @@ export default {
       this.loading = true;
       try {
         const result = await getAllJobs();
-        this.jobs = result.data;
+        this.jobs = result.data.data;
+        this.filteredJobs = this.jobs
       } catch (error) {
         this.error = error.message
       } finally {
         this.loading = false
       }
+    },
+    filterJobs(filter) {
+      if (filter === 'all') this.filteredJobs = this.jobs
+      else if (filter === 'fulltime') this.filteredJobs = this.jobs.filter(job => job.type === 'fulltime')
+      else if (filter === 'parttime') this.filteredJobs = this.jobs.filter(job => job.type === 'parttime')
+      else if (filter === 'onetime') this.filteredJobs = this.jobs.filter(job => job.type === 'onetime')
     }
   },
   mounted() {
@@ -33,8 +42,9 @@ export default {
 
 <template>
   <section>
+    <JobTypeNavbar @newFilter="filterJobs"/>
     <div class="row">
-      <div class="col-12 col-xl-2 col-lg-4 col-md-6 col-sm-6" v-for="job in jobs">
+      <div class="col-12 col-xl-2 col-lg-4 col-md-6 col-sm-6" v-for="job in filteredJobs">
           <JobCard :job="job"/>
       </div>
     </div>

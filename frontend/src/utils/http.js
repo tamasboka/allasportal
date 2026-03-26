66f070b48b03f7fbc127e6d4980afc33c43f89ba
Rendszergazda <rendszergazda@blathy.info>
2026-03-26 13:54:21 +0100

    add missing http:// to url

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-
 export const http = axios.create({
-    baseURL: '127.0.0.1:8000',
+    baseURL: 'http://127.0.0.1:8000',
     headers: {
-        "Content-Type":"application/json"
+        "Content-Type": "application/json"
     }
 });

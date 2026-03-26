f0dae9aebc2e62d9ad68b00c60fa7ef1aaeffcd2
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 20:38:33 +0100

    feat: add founded_at column

--- backend/database/migrations/2026_02_18_193925_create_organizations_table.php
+++ backend/database/migrations/2026_02_18_193925_create_organizations_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('founded_at');
             $table->timestamps();
         });
 

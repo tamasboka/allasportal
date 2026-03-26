153107d8df3aae9de72d6ed2771d859597fa4a95
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 20:41:58 +0100

    feat: make 2 organizations in seeder

--- backend/database/seeders/OrganizationSeeder.php
+++ backend/database/seeders/OrganizationSeeder.php
@@ -14,6 +14,18 @@ class OrganizationSeeder extends Seeder
      */
     public function run(): void
     {
+        Organization::create([
+            'name' => 'JobNest',
+            'founded_at' => now()
+        ]);
 
+        DB::table('organization_user')->insert([
+            'organization_id' => 1,
+            'user_id' => 1,
+        ]);
+        DB::table('organization_user')->insert([
+            'organization_id' => 1,
+            'user_id' => 2,
+        ]);
     }
 }

c508876c8fd60cc9c050b4aa9c7a5d234e4ce049
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 20:45:42 +0100

    feat: create auth:sanctum middleware for store, update, destroy functions

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\api\JobController;
+use App\Http\Controllers\api\OrganizationController;
 use App\Http\Controllers\api\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/jobs', JobController::class);
+Route::apiResource('/organizations', OrganizationController::class)
+    ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
 
-Route::apiResource('/user', UserController::class);
+Route::apiResource('/jobs', JobController::class)
+    ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
+
+Route::apiResource('/user', UserController::class)
+    ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');

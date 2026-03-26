2415698bb199d6a2803d9da206e79e20de2876b4
Bóka Tamás <tomi500offical@gmail.com>
2026-03-26 20:45:05 +0100

    feat: create basic User validation

---
+++ backend/app/Http/Requests/UserRequest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => [
+                'required',
+                'string',
+                'min:2',
+                'max:25'
+            ],
+            'lastname' => [
+                'required',
+                'string',
+                'min:2',
+                'max:25'
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'unique:users,email'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'max:25'
+            ],
+            'gender' => [
+                'string',
+                'in:male,female'
+            ],
+            'birthdate' => [
+                'date',
+            ],
+            'phone' => [
+                'string',
+                'min:8',
+                'max:15'
+            ],
+            'work_experience' => [
+                'numeric',
+                'min:0',
+            ],
+            'bio' => [
+                'string',
+                'min:1',
+                'max:1000'
+            ]
+        ];
+    }
+}

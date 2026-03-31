983b8d01d2f8018fc76f7d50a33d9c22f1ba923c
Rendszergazda <rendszergazda@blathy.info>
2026-03-31 10:40:48 +0200

    create controller

---
+++ backend/app/Http/Controllers/api/JobApplicationController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\JobApplicationRequest;
+use App\Http\Resources\JobApplication\JobApplicationCollection;
+use App\Http\Resources\JobApplication\JobApplicationResource;
+use App\Models\JobApplication;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class JobApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->user()->tokenCan('admin')) {
+            $applications = JobApplication::with('*')->get();
+            return (new JobApplicationCollection($applications))
+                ->response()
+                ->setStatusCode(200);
+        } else {
+            return response()
+                ->json([
+                    "message" => "Unauthorized"
+                ], 401);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(JobApplicationRequest $request)
+    {
+        if ($request->user()->tokenCan('user')) {
+            $application = JobApplication::create($request->all());
+            return (new JobApplicationResource($application))
+                ->response()
+                ->setStatusCode(201);
+        } else {
+            return response()
+                ->json([
+                    "message" => "Unauthorized"
+                ], 401);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $application = JobApplication::with('*')->findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()
+                ->json([
+                    "message" => "Job Application not found"
+                ], 404);
+        }
+        return (new JobApplicationResource($application))
+            ->response()
+            ->setStatusCode(200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(JobApplicationRequest $request, string $id)
+    {
+        try {
+            $application = JobApplication::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()
+                ->json([
+                    "message" => "Job Application not found"
+                ], 404);
+        }
+        if ($request->user()->id === $application->user_id) {
+            $application->update($request->validated());
+            return response()->json([
+                "message" => "Job Application updated successfully"
+            ], 200);
+        } else {
+            return response()
+                ->json([
+                    "message" => "Unauthorized"
+                ], 401);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+
+    }
+}

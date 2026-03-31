9400c3f3f4ed1b053d050ffcf8d05b7e3a7194c4
Rendszergazda <rendszergazda@blathy.info>
2026-03-31 10:40:23 +0200

    add paths

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\Api\JobApplicationController;
 use App\Http\Controllers\api\JobController;
+use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\api\OrganizationController;
+use App\Http\Controllers\api\RatingController;
 use App\Http\Controllers\api\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
+Route::post('/roles', [AuthController::class, 'abilities'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('/organizations', OrganizationController::class)
     ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
@@ -12,4 +23,11 @@ Route::apiResource('/jobs', JobController::class)
     ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
 
 Route::apiResource('/user', UserController::class)
-    ->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
+    ->middlewareFor(['index', 'update', 'destroy'], 'auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/organizations', OrganizationController::class);
+    Route::apiResource('jobapplications', JobApplicationController::class);
+    Route::apiResource('notifications', NotificationController::class);
+    Route::apiResource('ratings', RatingController::class);
+});

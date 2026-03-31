414be25f405d5ec3f12453c3ee190bcbace08cd0
Rendszergazda <rendszergazda@blathy.info>
2026-03-31 10:38:59 +0200

    add additional fields to math relations: saved_jobs, received_ratings, sent_notifications, received_notifications, sent_ratings, workplace

--- backend/app/Http/Resources/User/UserResource.php
+++ backend/app/Http/Resources/User/UserResource.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Resources\User;
 
+use App\Http\Resources\Job\JobCollection;
+use App\Http\Resources\Notification\NotificationCollection;
+use App\Http\Resources\Organization\OrganizationResource;
+use App\Http\Resources\Rating\RatingCollection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -23,7 +27,13 @@ class UserResource extends JsonResource
             'role' => $this->role,
             'phone' => $this->phone,
             'work_experience' => $this->work_experience,
-            'bio' => $this->bio
+            'bio' => $this->bio,
+            'saved_jobs' => new JobCollection($this->whenLoaded('saved_jobs')),
+            'received_ratings' => new RatingCollection($this->whenLoaded('received_ratings')),
+            'sent_notifications' => new NotificationCollection($this->whenLoaded('sent_notifications')),
+            'received_notifications' => new NotificationCollection($this->whenLoaded('received_notifications')),
+            'sent_ratings' => new RatingCollection($this->whenLoaded('sent_ratings')),
+            'workplace' => new OrganizationResource($this->whenLoaded('workplace')),
         ];
     }
 }

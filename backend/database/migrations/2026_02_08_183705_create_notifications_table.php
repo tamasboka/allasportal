fd006906301787fb4da01c355db195a9db1efe5f
Rendszergazda <rendszergazda@blathy.info>
2026-03-31 10:36:12 +0200

    add new 'general' type

--- backend/database/migrations/2026_02_08_183705_create_notifications_table.php
+++ backend/database/migrations/2026_02_08_183705_create_notifications_table.php
@@ -13,11 +13,11 @@ return new class extends Migration {
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
             $table->foreignIdFor(User::class, 'to_user_id');
             $table->foreignIdFor(User::class, 'from_user_id');
+            $table->string('title');
             $table->string('message');
-            $table->enum('type', ['accepted', 'rejected', 'system']);
+            $table->enum('type', ['accept', 'reject', 'general', 'system']);
             $table->boolean('is_read');
             $table->timestamps();
         });

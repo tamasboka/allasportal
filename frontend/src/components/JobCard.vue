13739122dd1c94eb9479bdf279c33b506b383915
BBence1 <beretzky.beni@gmail.com>
2026-04-04 18:19:24 +0200

    added the job type to the card and the RouterLink got a btn btn-outline-dark class. We made styles according to the job types

--- frontend/src/components/JobCard.vue
+++ frontend/src/components/JobCard.vue
@@ -7,22 +7,31 @@ export default {
       type: Object,
       required: true
     }
-  }
+  },
 }
 </script>
 
 <template>
   <section>
-    <div class="card">
+    <div class="card" :class="job.type">
       <div class="card-body">
         <h1 class="card-title text-dark">{{ job.name }}</h1>
+        <p class="card-text text-dark">({{job.type}})</p>
         <p class="card-text text-dark">{{ job.advertiser.firstname }} {{ job.advertiser.lastname }}</p>
-        <RouterLink :to="{name: 'job', params: {jobID: job.id}}">Részletek</RouterLink>
+        <RouterLink :to="{name: 'job', params: {jobID: job.id}}" class="btn btn-outline-dark">Részletek</RouterLink>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-
+.full-time{
+    border: 10px solid var(--bs-primary);
+}
+.part-time{
+    border: 10px solid var(--bs-secondary);
+}
+.one-time{
+  border: 10px solid #ffff47;
+}
 </style>

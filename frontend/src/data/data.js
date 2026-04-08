9e28ee3bb1e35704063506858abc7e75fa616b99
BBence1 <beretzky.beni@gmail.com>
2026-04-08 22:02:45 +0200

    created getJobById function

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -2,3 +2,7 @@ import {http} from '@/utils/http.js'
 export async function getAllJobs(){
     return await http.get('/api/jobs')
 }
+
+export async function getJobById(id){
+    return await  http.get('/api/jobs/'+id)
+}

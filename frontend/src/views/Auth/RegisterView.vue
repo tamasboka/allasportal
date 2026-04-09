62fbcc9629d5cc95f47f87b7ae4451b02f3afa7a
BBence1 <beretzky.beni@gmail.com>
2026-04-09 21:16:02 +0200

    made method and script

--- frontend/src/views/Auth/RegisterView.vue
+++ frontend/src/views/Auth/RegisterView.vue
@@ -1,35 +1,50 @@
 <script>
+import {Form,Field,ErrorMessage} from "vee-validate";
+import {http} from "@/utils/http.js";
 
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+  components: {Form,Field,ErrorMessage},
+  methods:{
+    async Register(data){
+      try{
+        await http.post('/api/register',data)
+      }catch (e){
+        console.log(e.message)
+      }
+      this.$router.push({
+        name: 'login'
+      })
+    }
+  }
 }
 </script>
 
 <template>
   <section class="mt-3">
-    <form>
+    <h1 class="text-center mb-4">Regisztráció</h1>
+    <Form @submit="Register">
       <div class="row">
         <div class="col-12 col-lg-6">
-          <input class="form-control" type="text" placeholder="Vezetéknév">
+          <Field name="firstname" class="form-control" type="text" placeholder="Vezetéknév"/>
         </div>
         <div class="col-12 col-lg-6">
-          <input class="form-control" type="text" placeholder="Keresztnév">
+          <Field name="lastname" class="form-control" type="text" placeholder="Keresztnév"/>
         </div>
       </div>
-      <input class="mt-3 form-control" type="email" placeholder="Email">
-      <input class="mt-3 form-control" type="password" placeholder="Jelszó">
-      <input class="mt-3 form-control" type="password" placeholder="Jelszó megerősítése">
-      <input class="mt-3 form-control" type="tel" placeholder="Telefonszám">
-      <select class="mt-3 form-select">
+      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email"/>
+      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó"/>
+      <Field name="phone" class="mt-3 form-control" type="tel" placeholder="Telefonszám"/>
+      <Field name="gender" as="select" class="mt-3 form-select">
         <option selected hidden="hidden">Neme</option>
-        <option>Nő</option>
-        <option>Férfi</option>
-        <option>Nem adom meg</option>
-      </select>
+        <option value="female">Nő</option>
+        <option value="male">Férfi</option>
+        <option value="not-given">Nem adom meg</option>
+      </Field>
       <div class="d-flex justify-content-center">
         <button class="mt-3 btn btn-success" type="submit">Regisztrálás</button>
       </div>
-    </form>
+    </Form>
   </section>
 </template>
 

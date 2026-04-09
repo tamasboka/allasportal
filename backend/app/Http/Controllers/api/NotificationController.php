20baca8575a2292e298f1c41d77590ff244ce2e3
Bóka Tamás <tomi500offical@gmail.com>
2026-04-09 21:37:29 +0200

    index, store, show, destroy functions

--- backend/app/Http/Controllers/api/NotificationController.php
+++ backend/app/Http/Controllers/api/NotificationController.php
@@ -1,8 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NotificationRequest;
+use App\Http\Resources\Notification\NotificationCollection;
+use App\Http\Resources\Notification\NotificationResource;
+use App\Models\Notification;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
@@ -12,23 +17,60 @@ class NotificationController extends Controller
      */
     public function index(Request $request)
     {
-
+        if ($request->user()->tokenCan('user')) {
+            $notifications = Notification::where('to_user_id', $request->user()->id)->with(['from'])->get();
+            return (new NotificationCollection($notifications))
+                ->response()
+                ->setStatusCode(200);
+        } else {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NotificationRequest $request)
     {
-        //
+        if ($request->user()->tokenCan('user')) {
+            $validated = $request->validated();
+            $notif = Notification::create($validated);
+            return response()->json([
+                $notif => new NotificationResource($notif)
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id, Request $request)
     {
-        //
+        try {
+            $notification = Notification::with(['to', 'from'])->findOrFail($id);
+            if ($request->user()->tokenCan('user') && ($request->user()->id === $notification->to_user_id || $request->user()->id === $notification->from_user_id)) {
+                if ($request->user()->role !== "admin" && $notification->from_user_id !== $request->user()->id) {
+                    $notification->update(['is_read' => 1]);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'Unauthorized'
+                ], 401);
+            }
+            return (new NotificationResource($notification))
+                ->response()
+                ->setStatusCode(200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Notification not found'
+            ], 404);
+        }
     }
 
     /**
@@ -42,8 +84,16 @@ class NotificationController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id, Request $request)
     {
-        //
+        try {
+            $notification = Notification::findOrFail($id);
+            $notification->delete();
+            return response()->json([], 204);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Notification not found'
+            ], 404);
+        }
     }
 }

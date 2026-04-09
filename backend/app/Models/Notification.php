47d11cab2aaa6801b9425744387d56298c714283
Bóka Tamás <tomi500offical@gmail.com>
2026-04-09 21:37:02 +0200

    add fillable columns

--- backend/app/Models/Notification.php
+++ backend/app/Models/Notification.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
+    protected $fillable = [
+        'to_user_id',
+        'from_user_id',
+        'title',
+        'message',
+        'type',
+        'is_read',
+    ];
     public $table = 'notifications';
     public function to() {
         return $this->belongsTo(User::class, 'to_user_id');

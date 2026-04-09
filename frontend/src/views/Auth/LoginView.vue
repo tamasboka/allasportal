2ab2d8f537d26ddc05e17aa70b8cf53ceca18e9c
BBence1 <beretzky.beni@gmail.com>
2026-04-09 21:14:43 +0200

    made login template, script and method

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -1,14 +1,36 @@
 <script>
+import {Form,Field,ErrorMessage} from "vee-validate";
+import {http} from "@/utils/http.js";
 
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  components: {Form,Field,ErrorMessage},
+  methods:{
+    async Login(data){
+      try{
+        await http.post('/api/login',data)
+      }catch (e){
+        console.log(e.message)
+      }
+      this.$router.push({
+        name: 'home'
+      })
+    }
+  }
 }
 </script>
 
 <template>
-
+  <section class="mt-3">
+    <h1 class="text-center mb-4">Bejelentkezés</h1>
+    <Form @submit="Login">
+      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email"/>
+      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó"/>
+      <div class="d-flex justify-content-center">
+        <button class="mt-3 btn btn-success" type="submit">Bejelentezés</button>
+      </div>
+    </Form>
+  </section>
 </template>
-
 <style scoped>
-
 </style>

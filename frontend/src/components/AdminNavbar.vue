2d9b1bb252db61065cb8c118ae24aadda9b751f2
BBence1 <beretzky.beni@gmail.com>
2026-04-13 11:13:56 +0200

    made a navbar for admin

---
+++ frontend/src/components/AdminNavbar.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+  name: "AdminNavbar"
+}
+</script>
+
+<template>
+  <header>
+    <nav class="navbar bg-black navbar-expand-lg p-3" data-bs-theme="dark">
+      <div class="container">
+        <RouterLink class="navbar-brand" :to="{name: 'admin-home'}">Admin főoldal</RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'admin-jobs'}">Állások</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'admin-companies'}">Cégek</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'admin-users'}">Felhasználók</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>

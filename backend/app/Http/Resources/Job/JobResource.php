8da0bc3101aba886564da6372b4d2b9708e00f70
Bóka Tamás <tomi500offical@gmail.com>
2026-04-15 14:52:38 +0200

    add every Job relation to resource

--- backend/app/Http/Resources/Job/JobResource.php
+++ backend/app/Http/Resources/Job/JobResource.php
@@ -3,8 +3,10 @@
 namespace App\Http\Resources\Job;
 
 use App\Http\Resources\Category\CategoryCollection;
+use App\Http\Resources\JobApplication\JobApplicationCollection;
 use App\Http\Resources\Rating\RatingCollection;
 use App\Http\Resources\Skill\SkillCollection;
+use App\Http\Resources\User\UserCollection;
 use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,15 +22,18 @@ class JobResource extends JsonResource
     {
         return [
             "id" => $this->id,
-            "advertiser" => new UserResource($this->whenLoaded('owner')),
             "name" => $this->name,
             "job_type" => $this->job_type,
-            "categories" => new CategoryCollection($this->whenLoaded('categories')),
-            "skills" => new SkillCollection($this->whenLoaded('required_skills')),
+            "capacity" => $this->capacity,
             "type" => $this->type,
             "min_salary" => $this->min_salary,
             "max_salary" => $this->max_salary,
-            'ratings' => new RatingCollection($this->whenLoaded('ratings')),
+            "advertiser" => new UserResource($this->whenLoaded('owner')),
+            "applications" => new JobApplicationCollection($this->whenLoaded('received_applications')),
+            "categories" => new CategoryCollection($this->whenLoaded('categories')),
+            "workers" => new UserCollection($this->whenLoaded('workers')),
+            "skills" => new SkillCollection($this->whenLoaded('required_skills')),
+            "ratings" => new RatingCollection($this->whenLoaded('ratings')),
         ];
     }
 }

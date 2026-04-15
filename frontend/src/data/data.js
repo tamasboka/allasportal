94c42726af4d1aedd9d708f7b2dc3d8e8d2c8125
BBence1 <beretzky.beni@gmail.com>
2026-04-15 20:29:13 +0200

    made getAllUsers function, getAllCategories function and getAllSkills function

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -6,3 +6,19 @@ export async function getAllJobs(){
 export async function getJobById(id){
     return await  http.get('/api/jobs/'+id)
 }
+
+export async function getAllUsers(){
+    return await http.get('/api/user',{
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+}
+
+export async function getAllCategories(){
+    return await http.get('/api/categories')
+}
+
+export async function getAllSkills(){
+    return await http.get('/api/skills')
+}

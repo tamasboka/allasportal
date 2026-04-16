1f92ad1cf485999da5d3585ed686081d8f408a9a
Bóka Tamás <tomi500offical@gmail.com>
2026-04-16 18:12:43 +0200

    seed categories, skills

--- backend/database/seeders/CategorySeeder.php
+++ backend/database/seeders/CategorySeeder.php
@@ -13,6 +13,29 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-
+        Category::create(['name' => 'Szoftverfejlesztés']);
+        Category::create(['name' => 'Adattudomány és Mesterséges Intelligencia']);
+        Category::create(['name' => 'Rendszergazda és Felhőkezelés']);
+        Category::create(['name' => 'Kiberbiztonság']);
+        Category::create(['name' => 'UI/UX Design']);
+        Category::create(['name' => 'Pénzügy és Könyvelés']);
+        Category::create(['name' => 'Emberi Erőforrások (HR)']);
+        Category::create(['name' => 'Adminisztráció és Asszisztencia']);
+        Category::create(['name' => 'Jog és Megfelelőség']);
+        Category::create(['name' => 'Bank és Biztosítás']);
+        Category::create(['name' => 'Marketing és Kommunikáció']);
+        Category::create(['name' => 'Értékesítés és Sales']);
+        Category::create(['name' => 'Tartalomgyártás és Copywriting']);
+        Category::create(['name' => 'Grafikai tervezés és Videóvágás']);
+        Category::create(['name' => 'Ügyfélszolgálat']);
+        Category::create(['name' => 'Gyártás és Termelés']);
+        Category::create(['name' => 'Szállítás és Logisztika']);
+        Category::create(['name' => 'Építőipar és Ingatlan']);
+        Category::create(['name' => 'Vendéglátás és Turizmus']);
+        Category::create(['name' => 'Mezőgazdaság és Élelmiszeripar']);
+        Category::create(['name' => 'Oktatás és Képzés']);
+        Category::create(['name' => 'Egészségügy és Gyógyszeripar']);
+        Category::create(['name' => 'Szépségápolás és Well-being']);
+        Category::create(['name' => 'Környezetvédelem és Fenntarthatóság']);
     }
 }
--- backend/database/seeders/SkillSeeder.php
+++ backend/database/seeders/SkillSeeder.php
@@ -13,6 +13,30 @@ class SkillSeeder extends Seeder
      */
     public function run(): void
     {
-
+        Skill::create(['name' => 'PHP (Laravel)']);
+        Skill::create(['name' => 'JavaScript (React/Vue)']);
+        Skill::create(['name' => 'Python']);
+        Skill::create(['name' => 'Docker & Kubernetes']);
+        Skill::create(['name' => 'SQL & NoSQL']);
+        Skill::create(['name' => 'Figma']);
+        Skill::create(['name' => 'Bérszámfejtés']);
+        Skill::create(['name' => 'Projektmenedzsment']);
+        Skill::create(['name' => 'Pénzügyi elemzés']);
+        Skill::create(['name' => 'Munkajogi ismeretek']);
+        Skill::create(['name' => 'Tárgyalástechnika']);
+        Skill::create(['name' => 'Google Ads / Facebook Ads']);
+        Skill::create(['name' => 'SEO optimalizálás']);
+        Skill::create(['name' => 'Adobe Creative Cloud']);
+        Skill::create(['name' => 'Copywriting']);
+        Skill::create(['name' => 'CRM rendszerek kezelése']);
+        Skill::create(['name' => 'Raktárkezelés']);
+        Skill::create(['name' => 'Targoncavezetői jogosítvány']);
+        Skill::create(['name' => 'Minőségbiztosítás']);
+        Skill::create(['name' => 'Beszerzési ismeretek']);
+        Skill::create(['name' => 'CAD tervezés']);
+        Skill::create(['name' => 'Idegen nyelv oktatás']);
+        Skill::create(['name' => 'Szakápolói ismeretek']);
+        Skill::create(['name' => 'Előadástechnika']);
+        Skill::create(['name' => 'Elsősegélynyújtás']);
     }
 }

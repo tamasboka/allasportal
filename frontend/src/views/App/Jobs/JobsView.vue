9513f1c3205640e21c0db28ce3644ad8977f4d62
Bóka Tamás <tomi500offical@gmail.com>
2026-04-18 16:38:36 +0200

    implement notification of empty content

--- frontend/src/views/App/Jobs/JobsView.vue
+++ frontend/src/views/App/Jobs/JobsView.vue
@@ -12,7 +12,8 @@ export default {
       jobs: [],
       filteredJobs: [],
       loading: false,
-      error: ''
+      error: '',
+      filtered: false
     }
   },
   methods: {
@@ -29,10 +30,19 @@ export default {
       }
     },
     filterJobs(filter) {
-      if (filter === 'all') this.filteredJobs = this.jobs
-      else if (filter === 'full-time') this.filteredJobs = this.jobs.filter(job => job.type === 'full-time')
-      else if (filter === 'part-time') this.filteredJobs = this.jobs.filter(job => job.type === 'part-time')
-      else if (filter === 'one-time') this.filteredJobs = this.jobs.filter(job => job.type === 'one-time')
+      if (filter === 'all') {
+        this.filtered = false
+        this.filteredJobs = this.jobs
+      } else if (filter === 'full-time') {
+        this.filtered = true
+        this.filteredJobs = this.jobs.filter(job => job.type === 'full-time')
+      } else if (filter === 'part-time') {
+        this.filtered = true
+        this.filteredJobs = this.jobs.filter(job => job.type === 'part-time')
+      } else if (filter === 'one-time') {
+        this.filtered = true
+        this.filteredJobs = this.jobs.filter(job => job.type === 'one-time')
+      }
     },
 
   },
@@ -48,12 +58,27 @@ export default {
   </section>
   <section v-else>
     <JobTypeNavbar @newFilter="filterJobs"/>
-    <div class="row p-5 m-1">
-      <div class="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-6" v-for="job in filteredJobs">
-          <JobCard :job="job"/>
+    <div v-if="!jobs.length" class="min-vh-100 d-flex justify-content-center align-items-center">
+      <div class="border-5 border-light rounded-3">
+        <h1>Még senki nem rakott fel ajánlást!</h1>
+        <RouterLink class="h4 text-success" :to="{name: 'create-job'}">Legyél az első!</RouterLink>
       </div>
     </div>
-
+    <div v-else-if="filtered && !filteredJobs.length">
+      <div class="min-vh-100 d-flex justify-content-center align-items-center">
+        <div class="border-5 border-light rounded-3">
+          <h1>Még senki nem rakott fel ilyen típusú ajánlást!</h1>
+          <RouterLink class="h4 text-success" :to="{name: 'create-job'}">Legyél az első!</RouterLink>
+        </div>
+      </div>
+    </div>
+    <div class="container" v-if="jobs.length !== 0">
+      <div class="row p-5">
+        <div class="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-5" v-for="job in filteredJobs">
+          <JobCard :job="job"/>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 

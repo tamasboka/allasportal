ec0eb3be0352664690de320441ffe29c506e7f9f
Bóka Tamás <tomi500offical@gmail.com>
2026-04-18 16:40:48 +0200

    add authentication, add EditWorkersForm and IncomingApplications components

--- frontend/src/views/User/Actions/EditJob.vue
+++ frontend/src/views/User/Actions/EditJob.vue
@@ -3,23 +3,54 @@ import NewJobForm from "@/components/NewJobForm.vue";
 import {Field, Form} from "vee-validate";
 import EditJobForm from "@/components/EditJobForm.vue";
 import EditWorkersForm from "@/components/EditWorkersForm.vue";
+import {http} from "@/utils/http.js";
+import Unauthorized from "@/views/Error/Unauthorized.vue";
+import ErrorLayout from "@/layouts/ErrorLayout.vue";
+import IncomingApplications from "@/components/IncomingApplications.vue";
 
 export default {
   name: "EditJob",
-  components: {EditWorkersForm, EditJobForm, NewJobForm, Field, Form},
+  components: {IncomingApplications, ErrorLayout, Unauthorized, EditWorkersForm, EditJobForm, NewJobForm, Field, Form},
   data() {
     return {
       isReturnBtnHovered: false,
       job: this.$route.meta.prefetched.job.data.data,
+      userLoading: false,
+      user: {}
     }
+  },
+  computed: {
+    isOwner() {
+      if (!this.user || !this.job || !this.job.advertiser) return false;
+      return this.user.id === this.job.advertiser.id;
+    }
+  },
+  methods: {
+    async getUserData() {
+      this.userLoading = true
+      if (!localStorage.getItem('token')) return
+      const res = await http.get('/api/me', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      this.userLoading = false
+      this.user = res.data.data
+    }
+  },
+  mounted() {
+    this.getUserData()
   }
 }
 </script>
 
 <template>
   <main>
-    <section class="min-vh-100 d-flex align-items-center justify-content-center">
-      <div class="card text-bg-dark">
+    <section class="min-vh-100 d-flex align-items-center justify-content-center" v-if="userLoading">
+      <h1 class="bg-danger">Authenticating</h1>
+    </section>
+    <section class="min-vh-100 d-flex align-items-center justify-content-center" v-if="isOwner">
+      <div class="card text-bg-dark border border-secondary rounded-5 shadow-lg">
         <div class="card-body">
           <RouterLink to="/" class="text-decoration-none text-light btn"
                       :class="{ 'btn-outline-light': !isReturnBtnHovered, 'btn-outline-primary': isReturnBtnHovered}"
@@ -28,9 +59,13 @@ export default {
             Vissza
           </RouterLink>
           <EditJobForm :job="job" class="mt-3"/>
-          <EditWorkersForm :workers="job.workers"/>
+          <EditWorkersForm :jobID="job.id" :workers="job.workers"/>
+          <IncomingApplications :jobID="job.id"/>
         </div>
       </div>
+    </section>
+    <section v-if="!isOwner" class="min-vh-100 d-flex justify-content-center align-items-center">
+      <Unauthorized/>
     </section>
   </main>
 </template>

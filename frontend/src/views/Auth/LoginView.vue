b4ac185c813254b293957c097bb3c3391786a0c5
Bóka Tamás <tomi500offical@gmail.com>
2026-04-18 16:35:40 +0200

    implement validation

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -1,21 +1,30 @@
 <script>
-import {Form,Field,ErrorMessage} from "vee-validate";
+import {Form, Field, ErrorMessage} from "vee-validate";
 import {http} from "@/utils/http.js";
 
 export default {
   name: "LoginView",
-  components: {Form,Field,ErrorMessage},
-  methods:{
-    async Login(data){
-      try{
-        const result = await http.post('/api/login',data)
-        localStorage.setItem('token',result.data.token)
-      }catch (e){
-        console.log(e.message)
+  data() {
+    return {
+      failed: false
+    }
+  },
+  components: {Form, Field, ErrorMessage},
+  methods: {
+    async Login(data) {
+      try {
+        const result = await http.post('/api/login', data)
+        if (result.data.token) {
+          localStorage.setItem('token', result.data.token)
+          this.$router.push({
+            name: 'home'
+          })
+        } else {
+          this.failed = true
+        }
+      } catch (e) {
+        this.failed = true
       }
-      this.$router.push({
-        name: 'home'
-      })
     }
   }
 }
@@ -24,9 +33,13 @@ export default {
 <template>
   <section class="mt-3">
     <h1 class="text-center mb-4">Bejelentkezés</h1>
+    <p class="alert alert-danger text-center mb-4" v-if="failed">Sikertelen bejelentkezés!</p>
     <Form @submit="Login">
-      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email"/>
-      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó"/>
+      <ErrorMessage class="alert alert-danger text-center" name="email" as="p"/>
+      <ErrorMessage class="alert alert-danger text-center" name="password" as="p"/>
+      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email" rules="required|email"/>
+      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó" rules="required|min:8|max:25"/>
+      <span class="text-center m-5">Még nincs fiókod? <RouterLink :to="{name: 'register'}" class="text-decoration-none">Regisztálj!</RouterLink></span>
       <div class="d-flex justify-content-center">
         <button class="mt-3 btn btn-warning" type="submit">Bejelentezés</button>
       </div>
--- frontend/src/views/Auth/RegisterView.vue
+++ frontend/src/views/Auth/RegisterView.vue
@@ -1,20 +1,26 @@
 <script>
-import {Form,Field,ErrorMessage} from "vee-validate";
+import {Form, Field, ErrorMessage} from "vee-validate";
 import {http} from "@/utils/http.js";
 
 export default {
   name: "RegisterView",
-  components: {Form,Field,ErrorMessage},
-  methods:{
-    async Register(data){
-      try{
-        await http.post('/api/register',data)
-      }catch (e){
+  data() {
+    return {
+      failed: false
+    }
+  },
+  components: {Form, Field, ErrorMessage},
+  methods: {
+    async Register(data) {
+      try {
+        await http.post('/api/register', data)
+        this.$router.push({
+          name: 'login'
+        })
+      } catch (e) {
         console.log(e.message)
+        this.failed = true
       }
-      this.$router.push({
-        name: 'login'
-      })
     }
   }
 }
@@ -23,20 +29,24 @@ export default {
 <template>
   <section class="mt-3">
     <h1 class="text-center mb-4">Regisztráció</h1>
+    <p class="alert alert-danger text-center mb-4" v-if="failed">Sikertelen regisztráció!</p>
     <Form @submit="Register">
+      <ErrorMessage class="alert alert-danger text-center" name="firstname" as="p"/>
+      <ErrorMessage class="alert alert-danger text-center" name="lastname" as="p"/>
+      <ErrorMessage class="alert alert-danger text-center" name="email" as="p"/>
+      <ErrorMessage class="alert alert-danger text-center" name="password" as="p"/>
       <div class="row">
         <div class="col-12 col-lg-6">
-          <Field name="firstname" class="form-control" type="text" placeholder="Vezetéknév"/>
+          <Field name="firstname" class="form-control" type="text" placeholder="Vezetéknév" rules="required|min:2|max:25"/>
         </div>
         <div class="col-12 col-lg-6">
-          <Field name="lastname" class="form-control" type="text" placeholder="Keresztnév"/>
+          <Field name="lastname" class="form-control" type="text" placeholder="Keresztnév" rules="required|min:2|max:25"/>
         </div>
       </div>
-      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email"/>
-      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó"/>
-      <Field name="phone" class="mt-3 form-control" type="tel" placeholder="Telefonszám"/>
+      <Field name="email" class="mt-3 form-control" type="email" placeholder="Email" rules="required|email"/>
+      <Field name="password" class="mt-3 form-control" type="password" placeholder="Jelszó" rules="required|min:8"/>
+      <Field name="phone" class="mt-3 form-control" type="tel" placeholder="Telefonszám" rules="min:8|max:15"/>
       <Field name="gender" as="select" class="mt-3 form-select">
-        <option selected hidden="hidden">Neme</option>
         <option value="female">Nő</option>
         <option value="male">Férfi</option>
         <option value="not-given">Nem adom meg</option>

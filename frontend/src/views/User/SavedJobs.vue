73483d6a19219e85847b0c952fe9d84e8622f90c
Bóka Tamás <tomi500offical@gmail.com>
2026-04-18 16:35:22 +0200

    create view for saved jobs

---
+++ frontend/src/views/User/SavedJobs.vue
@@ -0,0 +1,29 @@
+<script>
+import JobCard from "@/components/JobCard.vue";
+
+export default {
+  name: "SavedJobs",
+  components: {JobCard},
+  data() {
+    return {
+      jobs: this.$route.meta.prefetched.jobs
+    }
+  }
+}
+</script>
+
+<template>
+<section class="min-vh-100 d-flex justify-content-center py-5">
+  <div class="row">
+    <div class="col-12">
+      <RouterLink :to="{name: 'home'}" class="btn btn-outline-primary mb-5">Vissza</RouterLink>
+      <h1 class="border-bottom border-danger" v-if="!jobs.length">Nincsenek mentett állásaid!</h1>
+      <JobCard v-for="job in jobs" :job="job" class="mb-5" v-else/>
+    </div>
+  </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>

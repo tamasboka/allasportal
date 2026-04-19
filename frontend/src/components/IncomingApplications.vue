632428f52991be53d4f746da17677340f5cef0f1
Bóka Tamás <tomi500offical@gmail.com>
2026-04-19 21:24:06 +0200

    use JobApplication component

--- frontend/src/components/IncomingApplications.vue
+++ frontend/src/components/IncomingApplications.vue
@@ -1,9 +1,11 @@
 <script>
 import {getApplications} from "@/data/data.js";
 import {http} from "@/utils/http.js";
+import JobApplication from "@/components/JobApplication.vue";
 
 export default {
   name: "IncomingApplications",
+  components: {JobApplication},
   data() {
     return {
       applications: [],
@@ -64,20 +66,7 @@ export default {
     <h1 class="text-success text-center mb-4 fw-bold">Jelentkezők</h1>
     <h2 v-if="!applications.length" class="text-center">Nincsenek jelentkezők!</h2>
     <div v-else class="px-5">
-      <div v-for="application in applications" class="card w-100 border-5 border-secondary rounded-5">
-        <div class="card-body p-4">
-          <div class="d-flex border-bottom border-5 border-secondary">
-            <h2 class="h4">{{ application.sender.firstname }} {{ application.sender.lastname }}</h2>
-            <div class="ms-auto">
-              <button class="btn btn-success me-1" @click="acceptApplication(application.id)">Elfogadás</button>
-              <button class="btn btn-danger ms-1" @click="rejectApplication(application.id)">Elutasítás</button>
-            </div>
-          </div>
-          <div class="mt-3">
-            <p class="text-black">{{ application.message }}</p>
-          </div>
-        </div>
-      </div>
+      <JobApplication v-for="application in applications" @accept="acceptApplication" @reject="rejectApplication" :application="application" class="my-2"/>
     </div>
   </div>
 </template>

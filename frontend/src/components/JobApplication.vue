d382c2ff1dd4a24d881bcb3447bc89f78e5a991d
Bóka Tamás <tomi500offical@gmail.com>
2026-04-19 21:23:36 +0200

    create components for Review and JobApplication

---
+++ frontend/src/components/JobApplication.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "JobApplication",
+  props: {
+    application: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    accept(id) {
+      this.$emit('accept', id)
+    },
+    reject(id) {
+      this.$emit('reject', id)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card w-100 border-5 border-secondary rounded-5">
+    <div class="card-body p-4">
+      <div class="d-flex border-bottom border-5 border-secondary">
+        <h2 class="h4">{{ application.sender.firstname }} {{ application.sender.lastname }}</h2>
+        <div class="ms-auto">
+          <button class="btn btn-success me-1" @click="accept(application.id)">Elfogadás</button>
+          <button class="btn btn-danger ms-1" @click="reject(application.id)">Elutasítás</button>
+        </div>
+      </div>
+      <div class="mt-3">
+        <p class="text-black">{{ application.message }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Review.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "Review",
+  props: {
+    rating: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<p>{{ rating.title }}</p>
+<p>{{ rating.rating }}</p>
+<p>{{ rating.message }}</p>
+</template>
+
+<style scoped>
+
+</style>

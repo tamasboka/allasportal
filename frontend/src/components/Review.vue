d5185b38b1242f8b04e6b9ff505db6c3e3b207d2
BBence1 <beretzky.beni@gmail.com>
2026-04-20 15:11:30 +0200

    made review

--- frontend/src/components/Review.vue
+++ frontend/src/components/Review.vue
@@ -6,14 +6,20 @@ export default {
       type: Object,
       required: true
     }
-  }
+  },
 }
 </script>
 
 <template>
-<p>{{ rating.title }}</p>
-<p>{{ rating.rating }}</p>
-<p>{{ rating.message }}</p>
+  <div class="rounded-5 p-3 border border-3 border-secondary mb-3" >
+    <h3>{{rating.rater.firstname}} {{rating.rater.lastname}}</h3>
+    <p>{{ rating.title }}</p>
+    <div class="d-flex">
+      <i v-for="i in rating.stars" class="bi bi-star-fill text-warning"></i>
+    </div>
+    <p>{{ rating.message }}</p>
+  </div>
+
 </template>
 
 <style scoped>

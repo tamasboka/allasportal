99f768b6578b3e10a9ffbf1d5b2447572a2ddcb7
Bóka Tamás <tomi500offical@gmail.com>
2026-04-20 21:10:33 +0200

    slightly redesign profile card component, add skills

--- frontend/src/components/UserProfileCard.vue
+++ frontend/src/components/UserProfileCard.vue
@@ -16,15 +16,25 @@ export default {
 
 <template>
   <section>
-    <div class="border border-5 border-secondary rounded-5 p-3">
+    <div class="border border-5 border-secondary rounded-5 p-5">
       <h1 class="border-bottom border-5 border-secondary">{{ user.firstname }} {{ user.lastname }}</h1>
-      <div class="border-bottom border-5 border-secondary">
-        <p class="text-white text-center"><i class="bi bi-telephone"></i> +{{ user.phone }}</p>
-        <p class="text-white text-center"><i class="bi bi-envelope"></i> {{ user.email }}</p>
+      <div class="border-bottom border-5 border-secondary my-4">
+        <h2 class="text-center fw-bold h4 text-white">Elérhetőségek</h2>
+        <p class="text-white text-center" v-if="user?.phone"><i class="bi bi-telephone mx-2"></i>{{ user.phone }}</p>
+        <p class="text-white text-center"><i class="bi bi-envelope mx-2"></i>{{ user.email }}</p>
       </div>
-      <div>
+      <div class="my-4">
+        <h2 class="text-center fw-bold h4 text-white">{{ user.firstname }}-ról/ről</h2>
         <p v-if="!user.bio">Nincs leírás</p>
         <p v-else>{{ user.bio }}</p>
+      </div>
+      <div class="border-top border-secondary border-5 my-4"></div>
+      <h2 class="text-center fw-bold h4 text-white">Készségek</h2>
+      <p class="alert alert-warning text-center">Még nincsenek megadva készségek!</p>
+      <div class="d-flex flex-wrap gap-2">
+              <span v-for="skill in user.skills" :key="skill.id" class="badge rounded-pill bg-warning text-dark px-3">
+                {{ skill.name }}
+              </span>
       </div>
     </div>
   </section>

d20de3aa6286cc9896d3a5cb5990b109a7a49610
Bóka Tamás <tomi500offical@gmail.com>
2026-04-20 21:10:06 +0200

    redesign review component

--- frontend/src/components/Review.vue
+++ frontend/src/components/Review.vue
@@ -11,15 +11,20 @@ export default {
 </script>
 
 <template>
-  <div class="rounded-5 p-3 border border-3 border-secondary mb-3" >
-    <h3>{{rating.rater.firstname}} {{rating.rater.lastname}}</h3>
-    <p>{{ rating.title }}</p>
+  <div class="rounded-5 p-3 border border-3 border-secondary mb-3">
+    <h3>
+      <RouterLink :to="{name: 'user-home', params: {userID: rating.rater.id}}" class="text-decoration-none text-white">
+        <i class="bi bi-person"></i>{{ rating.rater.firstname }} {{ rating.rater.lastname }}
+      </RouterLink>
+    </h3>
     <div class="d-flex">
-      <i v-for="i in rating.stars" class="bi bi-star-fill text-warning"></i>
+      <i v-for="i in 5" class="bi bi-star-fill" :class="{'text-warning': i <= rating.stars, 'text-secondary': i > rating.stars}"></i>
     </div>
-    <p>{{ rating.message }}</p>
+    <div>
+      <h4 class="fw-bold">{{ rating.title }}</h4>
+      <p>{{ rating.message }}</p>
+    </div>
   </div>
-
 </template>
 
 <style scoped>

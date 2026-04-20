f56a9a89fb2f58b34a842ca550ae1055edb2eda5
BBence1 <beretzky.beni@gmail.com>
2026-04-20 15:12:32 +0200

    navbar and footer

--- frontend/src/layouts/UserLayout.vue
+++ frontend/src/layouts/UserLayout.vue
@@ -1,13 +1,19 @@
 <script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
 export default {
   name: "UserLayout",
+  components: {Footer, Navbar},
 }
 </script>
 
 <template>
+  <Navbar/>
   <main class="min-vh-100">
     <RouterView/>
   </main>
+  <Footer/>
 </template>
 
 <style scoped>

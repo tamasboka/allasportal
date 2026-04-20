aa1a795f96e5ac0657df20c23424b59ea1dab3f8
Bóka Tamás <tomi500offical@gmail.com>
2026-04-20 11:09:28 +0200

    finish index, show, update functions, move RatingRequest

--- backend/app/Http/Controllers/api/RatingController.php
+++ backend/app/Http/Controllers/api/RatingController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RatingRequest;
+use App\Http\Requests\Rating\RatingRequest;
+use App\Http\Resources\Rating\RatingCollection;
+use App\Http\Resources\Rating\RatingResource;
 use App\Models\Rating;
 use Illuminate\Http\Request;
 
@@ -12,9 +14,20 @@ class RatingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->user()->tokenCan('admin')) {
+            $ratings = Rating::with([
+                'rater',
+                'rated'
+            ])->get();
+            return (new RatingCollection($ratings))
+                ->response()
+                ->setStatusCode(200);
+        }
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
 
     /**
@@ -30,17 +43,34 @@ class RatingController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id, Request $request)
     {
-        //
+        if ($request->user()->tokenCan('admin')) {
+            $rating = Rating::with([
+                'rater',
+                'rated'
+            ])->findOrFail($id);
+            return (new RatingResource($rating))
+                ->response()
+                ->setStatusCode(200);
+        }
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(RatingRequest $request, string $id)
+    public function update(RatingRequest $request, Rating $rating)
     {
-        //
+        if ($request->user()->id === $rating->rater()->id) {
+            $validated = $request->validated();
+            $rating->update($validated);
+        }
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
 
     /**

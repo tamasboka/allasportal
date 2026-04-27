315be0f473edec85816b249a0ec5c3e6ec550e2a
BBence1 <beretzky.beni@gmail.com>
2026-04-27 11:36:12 +0200

    create About developers, tech stack, and an introduction

--- frontend/src/views/App/AboutUsView.vue
+++ frontend/src/views/App/AboutUsView.vue
@@ -10,41 +10,70 @@ export default {
 
     <!-- Ezeknek lehet kene kulon komponens -->
 
-    <div class="row text-center p-5 d-flex align-items-center text-white justify-content-center">
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h2">template question</h1>
+    <div class="border-block p-2">
+      <div class="underline-blue">
+        <h2 class="text-white text-center fw-bold">Miben segít ez az oldal?</h2>
       </div>
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h4">template answer</h1>
+      <p class="text-center">Az oldalunk lehetővé teszi a felhasználó számára, hogy könnyedén találjon, vagy közzé
+        tegyen új állásokat.</p>
+    </div>
+    <div class="border-block p-2 mt-5">
+      <div class="underline-blue">
+        <h2 class="text-white text-center fw-bold">Fejlesztők</h2>
+      </div>
+      <div class="row">
+        <div class="col-12 col-lg-6 col-md-6 col-sm-12">
+          <div class="inline-flex m-3 text-center">
+            <img src="https://avatars.githubusercontent.com/u/114821563?v=4" alt="tamasboka profile picture"
+                 class="mx-2">
+            <a href="https://github.com/tamasboka">Bóka Tamás</a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6 col-md-6 col-sm-12">
+          <div class="inline-flex m-3 text-center">
+            <img src="https://avatars.githubusercontent.com/u/171667151?v=4" alt="BBence1 profile picture" class="mx-2">
+            <a href="https://github.com/BBence1">Beretzky Bence</a>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row text-center p-5 d-flex align-items-center text-white justify-content-center">
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h2">template question</h1>
+    <div class="border-block p-2 mt-5">
+      <div class="underline-blue">
+        <h2 class="text-white text-center fw-bold">Tech stack</h2>
       </div>
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h4">template answer</h1>
-      </div>
-    </div>
-    <div class="row text-center p-5 d-flex align-items-center text-white justify-content-center">
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h2">template question</h1>
-      </div>
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h4">template answer</h1>
-      </div>
-    </div>
-    <div class="row text-center p-5 d-flex align-items-center text-white justify-content-center">
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h2">template question</h1>
-      </div>
-      <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5">
-        <h1 class="h4">template answer</h1>
+      <div class="row">
+        <div class="col-12 col-lg-4 col-md-6 col-sm-12">
+          <div class="inline-flex m-3 text-center">
+            <img src="https://icon.icepanel.io/Technology/svg/Vue.js.svg" alt="Vue.js icon">
+            <a href="https://vuejs.org/">Vue.js</a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 col-sm-12">
+          <div class="inline-flex m-3 text-center">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg" alt="Laravel icon">
+            <a href="https://laravel.com/">Laravel</a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 col-sm-12">
+          <div class="inline-flex m-3 text-center">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/MySQL_textlogo.svg/960px-MySQL_textlogo.svg.png" alt="Mysql icon">
+            <a href="https://www.mysql.com/">Mysql</a>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
+img {
+  width: 10%;
+  height: 10%;
+  border-radius: 50%;
+}
 
+a {
+  text-decoration: none;
+  color: white
+}
 </style>
